9500ae563729f4b633021974b53c58c954c1b7cf
noubar <noubar.kaloust@gmail.com>
2021-08-01 18:05:18 +0400

    dynamic add form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {Arr} from "../../Constants/constant";
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 
@@ -11,7 +10,20 @@ import {Arr} from "../../Constants/constant";
 })
 export class FormComponent implements OnInit {
   num : number = 1
-  arr = Arr
+  arr = new FormArray([new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    phoneNumber: new FormControl('', [Validators.required, Validators.pattern("0[0-9]{8}")]),
+    email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('', [Validators.required, Validators.pattern("^(?=.{6,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$")]),
+    confirmPassword: new FormControl('', Validators.required),
+  })])
 
 
 
@@ -19,8 +31,8 @@ export class FormComponent implements OnInit {
   password(i: number) {
     const password = this.arr.controls[i].get('password')?.value
     const  confirmedPassword  = this.arr.controls[i].get('confirmPassword')?.value
-    console.log(password)
-    return password === confirmedPassword ? false :  true ;
+    console.log(password,confirmedPassword)
+    return password !== confirmedPassword ;
   }
 
   constructor() {
@@ -30,7 +42,25 @@ export class FormComponent implements OnInit {
   }
 
   addForm() {
+    const form = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      phoneNumber: new FormControl('', [Validators.required, Validators.pattern("0[0-9]{8}")]),
+      email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z.-]+\\.[a-z]{2,4}$")]),
+      password: new FormControl('', [Validators.required, Validators.pattern("^(?=.{6,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$")]),
+      confirmPassword: new FormControl('', Validators.required),
+    })
     this.num += 1
+    if(this.arr.length < 10){
+      this.arr.push(form)
+    }
+
 
   }
 
